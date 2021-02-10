d19e4093a9f6ac19f84688824a2686d0ddc8c2bf
ilyas abduttawab <ilyasabduttawab@gmail.com>
2021-02-10 23:58:01 +0700

    add tables

--- app/controllers/ticket.controller.js
+++ app/controllers/ticket.controller.js
@@ -1,5 +1,7 @@
 const db = require("../models");
 const Ticket = db.ticket;
+const { Op } = require("sequelize");
+var sequelize = require("sequelize");
 
 exports.index = (req, res) => {
   Ticket.findAll().then((ticket) => {
@@ -12,26 +14,42 @@ exports.index = (req, res) => {
   });
 };
 
-exports.update = (req, res) => {
-    const id = req.params.id;
-    Ticket.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Ticket was updated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot update Ticket with id=${id}. Maybe Ticket was not found or req.body is empty!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Ticket with id=" + id
-        });
+exports.number = (req, res) => {
+  Ticket.findAll({
+    attributes: [
+      "priority",
+      [sequelize.fn("COUNT", sequelize.col("priority")), "total_tickets"],
+    ],
+    group: ["priority"],
+  }).then((number) => {
+    if (number) {
+      res.status(200).send({
+        data: number,
       });
-  };
-  
+      return;
+    }
+  });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Ticket.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Ticket was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Ticket with id=${id}. Maybe Ticket was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Ticket with id=" + id,
+      });
+    });
+};
--- app/routes/ticket.routes.js
+++ app/routes/ticket.routes.js
@@ -2,5 +2,6 @@ const controller = require("../controllers/ticket.controller");
 
 module.exports = function (app) {
   app.get("/api/tickets", controller.index);
+  app.get("/api/tickets/number", controller.number);
   app.put("/api/tickets/:id", controller.update);
 };
